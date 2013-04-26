11537befd6b2c61df4169bb1946611802f2e7d0e
Devon Campbell <raddevon@gmail.com>
2013-04-26 14:07:45 -0400

    Refactored expando

--- js/me.raddevon.js
+++ js/me.raddevon.js
@@ -1,9 +1,21 @@
 function expandoToggle (event) {
     event.preventDefault();
-    var expando = $('#' + $(this).data('expando'));
+    var expando = $($(this).data('expando'));
     expando.prependTo(expando.parent()).slideToggle(400, 'easeInOutQuart');
+}
+
+function expandoCloseAll (event) {
+    event.preventDefault();
+    var expandoContainer = $($(this).data('target'));
+    expandoContainer.find('.expando').each( function() {
+        if ($(this).css('display') !== 'none') {
+            expandoToggle();
+        }
+    });
 }
 
 $(document).ready( function() {
     $('[data-expando]').on('click', expandoToggle);
+    $('.expando-collapse').on('click', expandoCloseAll);
+    $('.expando').css('display', 'none');
 });
