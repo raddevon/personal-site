e9156bb6ea692b201b8521800ac293133494a024
Devon Campbell <raddevon@gmail.com>
2013-05-30 14:56:35 -0400

    Fixed header backgrounds in Safari

--- js/image-randomizer.js
+++ js/image-randomizer.js
@@ -1,4 +1,4 @@
 var images = 3;
 var currentBackground = Math.floor(Math.random() * images + 1);
 
-$('header').css('background', 'url(img/header-' + currentBackground + '.jpg) center / cover no-repeat fixed');
+$('header').addClass('background-' + currentBackground);
