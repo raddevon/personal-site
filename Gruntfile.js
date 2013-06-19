acdf3e33c95d53a355a1e28c5d42b3ea77fca7ec
Devon Campbell <raddevon@gmail.com>
2013-06-19 08:45:58 -0400

    Implemented grunt.js

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+var scripts = ['js/jquery-1.9.1.js', 'js/jquery.animate-enhanced.min.js','jquery.easing.min.js', 'js/**/*.js', '!js/scripts.js'];
+
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      options: {
+        force: true
+      },
+      all: scripts + ['!js/jquery-1.9.1.js', '!js/jquery.animate-enhanced.min.js', '!jquery.easing.min.js']
+    },
+
+    concat: {
+      dist: {
+        src: scripts,
+        dest: 'js/scripts.js'
+      }
+    },
+
+    uglify: {
+      min: {
+        files: {
+          'js/scripts.js': ['js/scripts.js']
+        }
+      }
+    },
+
+    compass: {
+      dev: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'css'
+        }
+      },
+      production: {
+        options: {
+          sassDir: 'sass',
+          cssDir: 'css',
+          environment: 'production',
+          outputStyle: 'compressed'
+        }
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      scripts: {
+        files: scripts,
+        tasks: ['jshint']
+      },
+      styles: {
+        files: ['sass/**/*.{sass,scss}'],
+        tasks: ['compass:dev']
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'compass:dev', 'watch']);
+  grunt.registerTask('build', ['jshint', 'compass:production', 'concat', 'uglify']);
+
+};
--- js/image-randomizer-ck.js
+++
@@ -1,1 +0,0 @@
-var images=3,currentBackground=Math.floor(Math.random()*images+1);$("header").css("background","url(img/header-"+currentBackground+".jpg) center / cover no-repeat fixed");
