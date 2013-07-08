fa7e10e682e8d6fff6916dfc9318a799e799cd40
Devon Campbell <raddevon@gmail.com>
2013-07-07 21:43:01 -0400

    Footer social links with JS replacement with icons

--- js/project.js
+++ js/project.js
@@ -12,3 +12,25 @@ $('a[href^="#"]').on('click',function (e) {
         });
     }
 });
+
+//Replace social links with icons
+$('#social-links').find('a[href]').each(function(i, e) {
+    // Maps social domains to the HTML to insert their respective icons
+    var socialIcons = {
+        'twitter.com': '<i class="icon-twitter"></i>',
+        'github.com': '<i class="icon-github"></i>',
+        'dribbble.com': '<i class="icon-dribbble"></i>',
+        'facebook.com': '<i class="icon-facebook"></i>',
+        'plus.google.com': '<i class="icon-gplus"></i>',
+        'linkedin.com': '<i class="icon-linkedin"></i>'
+    }
+    // Extracts the domain from the element's href property
+    var domain = e.href.split('/')[2]
+    // Loop through socialIcons looking for the domain in the element href's domain. Replace HTML if a match is found and end the loop.
+    for (var key in socialIcons) {
+        if (domain.indexOf(key) !== -1) {
+            $(e).html(socialIcons[key]);
+            break;
+        }
+    }
+});
